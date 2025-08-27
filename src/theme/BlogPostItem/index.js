ead8aea47a1963016f823994436f15e4aa73a04e
Marques <41787413+JLuisMarques@users.noreply.github.com>
2025-08-28 03:56:27 +0500

    Update index.js

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-export default function BlogPostItem({ children, frontMatter }) {
-  const { title, image } = frontMatter;
+export default function BlogPostItem(props) {
+  // Destructure frontMatter safely with a default empty object
+  const { frontMatter = {}, children } = props;
+  const { title = 'Untitled', image, description } = frontMatter;
 
   return (
     <article className={clsx('blog-post', styles.blogPost)}>
@@ -21,7 +23,13 @@ export default function BlogPostItem({ children, frontMatter }) {
         />
       )}
 
+      {/* Render title */}
       <h1>{title}</h1>
+
+      {/* Optional description */}
+      {description && <p>{description}</p>}
+
+      {/* Blog content */}
       <div className="blog-post-content">{children}</div>
     </article>
   );
